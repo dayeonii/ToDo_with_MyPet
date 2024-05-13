52f5d4725707b09d04ee758db34ece7aa7aa1ca5
dayeonii <daabbong@gmail.com>
2024-05-13 15:57:37 +0900

    1.Conceptualization ver 1.01 update

--- test_files/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("first commit success!");
-    return 0;
-}
---
+++ todo_with_mypet/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//위젯 추가
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:
+    )
+  }
+}
