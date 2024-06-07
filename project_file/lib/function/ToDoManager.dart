69cba72452e604b37a9922f11753be1d761e7bd2
dayeonii <daabbong@gmail.com>
2024-06-08 00:52:12 +0900

    ToDoManager.dart를 새로운 컬렉션에 접근하도록 수정

--- project_file/lib/function/ToDoManager.dart
+++ project_file/lib/function/ToDoManager.dart
@@ -1,19 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_pet/function/CheckToDo.dart';
 
 class ToDoManager {
+  //Firebase 정보를 가져오기 위한 인스턴스 객체
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //ToDos 로컬 저장
   List<Map<String, dynamic>> todoList = [];
   double progressRate = 0.0;
 
+  //생성자
   ToDoManager() {
     _loadToDos();
   }
 
+  //현재 로그인한 사용자의 userID 가져오기
+  String getUserID() {
+    return _auth.currentUser!.uid;
+  }
+
+  //Firebase에 저장된 해당 사용자의 ToDos 하위 컬렉션에 접근
+  CollectionReference getUserToDos() {
+    String userID = getUserID();
+    return _firestore.collection('USER').doc(userID).collection('ToDos');
+  }
+
+  //로컬 투두 리스트에 서버의 내용 불러와 저장하기
   Future<void> _loadToDos() async {
     try {
-      final QuerySnapshot result = await _firestore.collection('todos').get();
+      final QuerySnapshot result = await getUserToDos().get();
       final List<DocumentSnapshot> documents = result.docs;
       todoList = documents.map((doc) => {
         'id': doc.id,
@@ -41,7 +59,7 @@ class ToDoManager {
         'timestamp': FieldValue.serverTimestamp(),
       };
 
-      DocumentReference docRef = await _firestore.collection('todos').add(newToDo);
+      DocumentReference docRef = await getUserToDos().add(newToDo);
       newToDo['id'] = docRef.id;
       todoList.add(newToDo);
       _renewProgressRate();
@@ -53,7 +71,7 @@ class ToDoManager {
 
   Future<void> deleteToDo(String id) async {
     try {
-      await _firestore.collection('todos').doc(id).delete();
+      await getUserToDos().doc(id).delete();
       todoList.removeWhere((todo) => todo['id'] == id);
       _renewProgressRate();
       //print('Deleted todo with id: $id');
@@ -64,7 +82,7 @@ class ToDoManager {
 
   Future<void> completeToDo(String id) async {
     try {
-      await _firestore.collection('todos').doc(id).update({'isCompleted': true});
+      await getUserToDos().doc(id).update({'isCompleted': true});
       int index = todoList.indexWhere((todo) => todo['id'] == id);
       if (index != -1) {
         todoList[index]['isCompleted'] = true;
@@ -90,7 +108,7 @@ class ToDoManager {
   }
 
   Stream<QuerySnapshot> getTodosStream() {
-    return _firestore.collection('todos').orderBy('timestamp', descending: true).snapshots();
+    return getUserToDos().orderBy('timestamp', descending: true).snapshots();
   }
 
   Future<double> calculateProgressRate() async {
